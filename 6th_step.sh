9309c13cf7fd3a2a307b33b5b61ba8e4cf07a9ec
VizzleTF <54704529+VizzleTF@users.noreply.github.com>
2021-01-17 19:38:10 +0300

    Update 6th_step.sh

--- 6th_step.sh
+++ 6th_step.sh
@@ -19,10 +19,10 @@ interface=$(ext_interface);
 scp /etc/ufw/before.rules /etc/ufw/before.rules.bkp;
 
 sed '/*filter/ i *nat \
--A POSTROUTING -s 10.10.10.0/24 -o '$interface' -m policy --pol ipsec --dir out -j ACCEPT \
--A POSTROUTING -s 10.10.10.0/24 -o '$interface' -j MASQUERADE \
-COMMIT \
- \
+-A POSTROUTING -s 10.10.10.0/24 -o eth0 -m policy --pol ipsec --dir out -j ACCEPT \
+-A POSTROUTING -s 10.10.10.0/24 -o eth0 -j MASQUERADE \
+COMMIT\
+\
 *mangle \
 -A FORWARD --match policy --pol ipsec --dir in -s 10.10.10.0/24 -o eth0 -p tcp -m tcp --tcp-flags SYN,RST SYN -m tcpmss --mss 1361:1536 -j TCPMSS --set-mss 1360 \
 COMMIT' /etc/ufw/before.rules.bkp > /etc/ufw/before.rules;
