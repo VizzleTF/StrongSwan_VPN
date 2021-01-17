4cdfe32e39ffc2f29929e4b1eb46c90abf8137bb
VizzleTF <54704529+VizzleTF@users.noreply.github.com>
2021-01-17 20:44:29 +0300

    Update 2nd_step.sh

--- 2nd_step.sh
+++ 2nd_step.sh
@@ -8,18 +8,18 @@
 
 
 echo "making directory for certs.."
-mkdir -p ~/pki/{cacerts,certs,private}
+mkdir -p ~/pki/{cacerts,certs,private};
 echo "done"
 
 echo "change rights for directory.."
-chmod 700 ~/pki
+chmod 700 ~/pki;
 echo "done"
 
 echo "generate root key.."
-ipsec pki --gen --type rsa --size 4096 --outform pem > ~/pki/private/ca-key.pem
+ipsec pki --gen --type rsa --size 4096 --outform pem > ~/pki/private/ca-key.pem;
 echo "done"
 
 echo "making center of sertification.."
 ipsec pki --self --ca --lifetime 3650 --in ~/pki/private/ca-key.pem \
-	    --type rsa --dn "CN=github.com/VizzleTF" --outform pem > ~/pki/cacerts/ca-cert.pem
+	    --type rsa --dn "CN=github.com/VizzleTF" --outform pem > ~/pki/cacerts/ca-cert.pem;
 echo "done"
