f67a51d7b1c897a86306d5753f3194cb2289772b
VizzleTF <54704529+VizzleTF@users.noreply.github.com>
2021-01-18 10:24:11 +0300

    Update 3rd_step.sh

--- 3rd_step.sh
+++ 3rd_step.sh
@@ -14,19 +14,16 @@ echo " "
 echo "Please enter IP address of your VPN server:"
 read IP
 
-CN="CN=$IP" --san "$IP"
-echo "$CN"
+echo "Adding sertificate for $IP VPN server.."
+ipsec pki --pub --in ~/pki/private/server-key.pem --type rsa \
+	    | ipsec pki --issue --lifetime 1825 \
+	            --cacert ~/pki/cacerts/ca-cert.pem \
+		            --cakey ~/pki/private/ca-key.pem \
+			            --dn "CN=$IP" --san "$IP" \
+				            --flag serverAuth --flag ikeIntermediate --outform pem \
+ 				         >  ~/pki/certs/server-cert.pem;
 
-#echo "Adding sertificate for $IP VPN server.."
-#ipsec pki --pub --in ~/pki/private/server-key.pem --type rsa \
-#	    | ipsec pki --issue --lifetime 1825 \
-#	            --cacert ~/pki/cacerts/ca-cert.pem \
-#		            --cakey ~/pki/private/ca-key.pem \
-#			            --dn $CN \
-#				            --flag serverAuth --flag ikeIntermediate --outform pem \
-#					        >  ~/pki/certs/server-cert.pem;
-#
-#sudo cp -r ~/pki/* /etc/ipsec.d/
+sudo cp -r ~/pki/* /etc/ipsec.d/
 
 echo "done"
 echo " "
