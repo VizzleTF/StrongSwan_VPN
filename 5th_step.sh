465105d00dd17b1e3417de6a59ee3b696bb37a40
VizzleTF <54704529+VizzleTF@users.noreply.github.com>
2021-01-18 19:48:20 +0300

    Update 5th_step.sh

--- 5th_step.sh
+++ 5th_step.sh
@@ -1,9 +1,76 @@
 # setting autentification vpn
 
+echo "#############################"
+sleep 0.1
+echo "####                     ####"
+sleep 0.1
+echo "##                         ##"
+sleep 0.1
+echo "##  Adding server-key.pem  ##"
+sleep 0.1
+echo "##                         ##"
+sleep 0.1
+echo "####                     ####"
+sleep 0.1
+echo "#############################"
+sleep 0.5
+
 echo ": RSA \"server-key.pem\" " >> /etc/ipsec.secrets
-echo "enter your vpn user:"
+
+clear
+
+echo "Enter your vpn user:"
 read user
-echo "enter your password:"
+echo "Enter your password:"
 read password
 
+echo "#############################"
+sleep 0.1
+echo "####                     ####"
+sleep 0.1
+echo "##                         ##"
+sleep 0.1
+echo "##    Adding your user     ##"
+sleep 0.1
+echo "##        and pass         ##"
+sleep 0.1
+echo "####                     ####"
+sleep 0.1
+echo "#############################"
+sleep 0.5
+clear
+
 echo $user : EAP $password >>/etc/ipsec.secrets
+
+echo "#############################"
+sleep 0.1
+echo "####                     ####"
+sleep 0.1
+echo "##                         ##"
+sleep 0.1
+echo "##          Done           ##"
+sleep 0.1
+echo "##                         ##"
+sleep 0.1
+echo "####                     ####"
+sleep 0.1
+echo "#############################"
+sleep 0.5
+clear
+
+echo "#############################"
+sleep 0.1
+echo "####                     ####"
+sleep 0.1
+echo "##                         ##"
+sleep 0.1
+echo "##      Go to step 6!      ##"
+sleep 0.1
+echo "##                         ##"
+sleep 0.1
+echo "####                     ####"
+sleep 0.1
+echo "#############################"
+sleep 0.1
+echo "#############################"
+sleep 0.6
