b13eca7e66c70eb57ef021c9848eb4ac2b9873da
VizzleTF <54704529+VizzleTF@users.noreply.github.com>
2021-01-18 19:34:15 +0300

    Update 3rd_step.sh

--- 3rd_step.sh
+++ 3rd_step.sh
@@ -1,20 +1,55 @@
-##########################	
-#			 #
-# Generating sertificate #
-#     For VPN SERVER     #
-#			 #
-##########################
+clear
+echo "#############################"
+sleep 0.1
+echo "####                     ####"
+sleep 0.1
+echo "##                         ##"
+sleep 0.1
+echo "##  Generating close key.. ##"
+sleep 0.1
+echo "##                         ##"
+sleep 0.1
+echo "####                     ####"
+sleep 0.1
+echo "#############################"
+sleep 0.5
 
-
-echo "Generating close key.."
 ipsec pki --gen --type rsa --size 4096 --outform pem > ~/pki/private/server-key.pem;
-echo "done"
-echo " "
+
+echo "#############################"
+sleep 0.1
+echo "####                     ####"
+sleep 0.1
+echo "##                         ##"
+sleep 0.1
+echo "##          Done           ##"
+sleep 0.1
+echo "##                         ##"
+sleep 0.1
+echo "####                     ####"
+sleep 0.1
+echo "#############################"
+sleep 0.5
+clear
 
 echo "Please enter IP address of your VPN server:"
 read IP
 
-echo "Adding sertificate for $IP VPN server.."
+echo "#############################"
+sleep 0.1
+echo "####                     ####"
+sleep 0.1
+echo "##                         ##"
+sleep 0.1
+echo "##   Adding sertificate    ##"
+sleep 0.1
+echo "##  for your VPN server..  ##"
+sleep 0.1
+echo "####                     ####"
+sleep 0.1
+echo "#############################"
+sleep 0.5
+
 ipsec pki --pub --in ~/pki/private/server-key.pem --type rsa \
 	    | ipsec pki --issue --lifetime 1825 \
 	            --cacert ~/pki/cacerts/ca-cert.pem \
@@ -23,8 +58,69 @@ ipsec pki --pub --in ~/pki/private/server-key.pem --type rsa \
 				            --flag serverAuth --flag ikeIntermediate --outform pem \
  				         >  ~/pki/certs/server-cert.pem;
 
+echo "#############################"
+sleep 0.1
+echo "####                     ####"
+sleep 0.1
+echo "##                         ##"
+sleep 0.1
+echo "##          Done           ##"
+sleep 0.1
+echo "##                         ##"
+sleep 0.1
+echo "####                     ####"
+sleep 0.1
+echo "#############################"
+sleep 0.5
+clear
+
+echo "#############################"
+sleep 0.1
+echo "####                     ####"
+sleep 0.1
+echo "##                         ##"
+sleep 0.1
+echo "##  Copy to /etc/ipsec.d   ##"
+sleep 0.1
+echo "##                         ##"
+sleep 0.1
+echo "####                     ####"
+sleep 0.1
+echo "#############################"
+sleep 0.5
+
 sudo cp -r ~/pki/* /etc/ipsec.d/
 
-echo "done"
-echo " "
+echo "#############################"
+sleep 0.1
+echo "####                     ####"
+sleep 0.1
+echo "##                         ##"
+sleep 0.1
+echo "##          Done           ##"
+sleep 0.1
+echo "##                         ##"
+sleep 0.1
+echo "####                     ####"
+sleep 0.1
+echo "#############################"
+sleep 0.5
+clear
+
+echo "#############################"
+sleep 0.1
+echo "####                     ####"
+sleep 0.1
+echo "##                         ##"
+sleep 0.1
+echo "##      Go to step 4!      ##"
+sleep 0.1
+echo "##                         ##"
+sleep 0.1
+echo "####                     ####"
+sleep 0.1
+echo "#############################"
+sleep 0.1
+echo "#############################"
+sleep 0.6
 
