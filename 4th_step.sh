d5cb9f5a85ec6a3a9a7e70acf04c5268cd4cc151
VizzleTF <54704529+VizzleTF@users.noreply.github.com>
2021-01-18 19:40:50 +0300

    Update 4th_step.sh

--- 4th_step.sh
+++ 4th_step.sh
@@ -1,16 +1,53 @@
-##########################
-#			 #
-#   Setting StrongSwan   #
-#                        #
-##########################
+echo "#############################"
+sleep 0.1
+echo "####                     ####"
+sleep 0.1
+echo "##                         ##"
+sleep 0.1
+echo "##  Backing up ipsec.conf  ##"
+sleep 0.1
+echo "##                         ##"
+sleep 0.1
+echo "####                     ####"
+sleep 0.1
+echo "#############################"
+sleep 0.5
 
-echo "backing up original settings"
 sudo mv /etc/ipsec.conf{,.original}; 
-echo "done"
-echo "  "
+
+echo "#############################"
+sleep 0.1
+echo "####                     ####"
+sleep 0.1
+echo "##                         ##"
+sleep 0.1
+echo "##          Done           ##"
+sleep 0.1
+echo "##                         ##"
+sleep 0.1
+echo "####                     ####"
+sleep 0.1
+echo "#############################"
+sleep 0.5
+clear
 
 echo "Please enter IP address of your VPN server:"
 read IP
+
+echo "#############################"
+sleep 0.1
+echo "####                     ####"
+sleep 0.1
+echo "##                         ##"
+sleep 0.1
+echo "##  Setting up ipsec.conf  ##"
+sleep 0.1
+echo "##                         ##"
+sleep 0.1
+echo "####                     ####"
+sleep 0.1
+echo "#############################"
+sleep 0.5
 
 echo "config setup
     charondebug=\"ike 1, knl 1, cfg 0\"
@@ -38,3 +75,37 @@ conn ikev2-vpn
     rightdns=8.8.8.8,8.8.4.4
     rightsendcert=never
     eap_identity=%identity" > /etc/ipsec.conf
+
+
+echo "#############################"
+sleep 0.1
+echo "####                     ####"
+sleep 0.1
+echo "##                         ##"
+sleep 0.1
+echo "##          Done           ##"
+sleep 0.1
+echo "##                         ##"
+sleep 0.1
+echo "####                     ####"
+sleep 0.1
+echo "#############################"
+sleep 0.5
+clear
+
+echo "#############################"
+sleep 0.1
+echo "####                     ####"
+sleep 0.1
+echo "##                         ##"
+sleep 0.1
+echo "##      Go to step 5!      ##"
+sleep 0.1
+echo "##                         ##"
+sleep 0.1
+echo "####                     ####"
+sleep 0.1
+echo "#############################"
+sleep 0.1
+echo "#############################"
+sleep 0.6
