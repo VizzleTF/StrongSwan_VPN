29a3560ef7cdad4e7ff22aeed48dc7b2e8b1cc49
VizzleTF <54704529+VizzleTF@users.noreply.github.com>
2021-01-18 19:19:05 +0300

    Update 2nd_step.sh

--- 2nd_step.sh
+++ 2nd_step.sh
@@ -1,25 +1,150 @@
 #!/bin/bash
 
-##################################
-#				 #
-#   making certification center  #
-#				 #
-##################################
+clear
 
+echo "#############################"
+sleep 0.1
+echo "#############################"
+sleep 0.1
+echo "####                     ####"
+sleep 0.1
+echo "##                         ##"
+sleep 0.1
+echo "##     Making directory    ##"
+sleep 0.1
+echo "##        for certs..      ##"
+sleep 0.1
+echo "####                     ####"
+sleep 0.1
+echo "#############################"
+sleep 0.5
 
-echo "making directory for certs.."
 mkdir -p ~/pki/{cacerts,certs,private};
-echo "done"
 
-echo "change rights for directory.."
+echo "#############################"
+sleep 0.1
+echo "####                     ####"
+sleep 0.1
+echo "##                         ##"
+sleep 0.1
+echo "##          Done           ##"
+sleep 0.1
+echo "##                         ##"
+sleep 0.1
+echo "####                     ####"
+sleep 0.1
+echo "#############################"
+sleep 0.5
+
+echo "#############################"
+sleep 0.1
+echo "####                     ####"
+sleep 0.1
+echo "##                         ##"
+sleep 0.1
+echo "##     Change rights       ##"
+sleep 0.1
+echo "##     for directory..     ##"
+sleep 0.1
+echo "####                     ####"
+sleep 0.1
+echo "#############################"
+sleep 0.5
+
 chmod 700 ~/pki;
-echo "done"
 
-echo "generate root key.."
+echo "#############################"
+sleep 0.1
+echo "####                     ####"
+sleep 0.1
+echo "##                         ##"
+sleep 0.1
+echo "##          Done           ##"
+sleep 0.1
+echo "##                         ##"
+sleep 0.1
+echo "####                     ####"
+sleep 0.1
+echo "#############################"
+sleep 0.5
+
+echo "#############################"
+sleep 0.1
+echo "####                     ####"
+sleep 0.1
+echo "##                         ##"
+sleep 0.1
+echo "##   Generate root key..   ##"
+sleep 0.1
+echo "##                         ##"
+sleep 0.1
+echo "####                     ####"
+sleep 0.1
+echo "#############################"
+sleep 0.5
+
 ipsec pki --gen --type rsa --size 4096 --outform pem > ~/pki/private/ca-key.pem;
-echo "done"
 
-echo "making center of sertification.."
+echo "#############################"
+sleep 0.1
+echo "####                     ####"
+sleep 0.1
+echo "##                         ##"
+sleep 0.1
+echo "##          Done           ##"
+sleep 0.1
+echo "##                         ##"
+sleep 0.1
+echo "####                     ####"
+sleep 0.1
+echo "#############################"
+sleep 0.5
+
+echo "#############################"
+sleep 0.1
+echo "####                     ####"
+sleep 0.1
+echo "##                         ##"
+sleep 0.1
+echo "##      making center      ##"
+sleep 0.1
+echo "##    of sertification..   ##"
+sleep 0.1
+echo "####                     ####"
+sleep 0.1
+echo "#############################"
+sleep 0.5
+
 ipsec pki --self --ca --lifetime 3650 --in ~/pki/private/ca-key.pem \
-	    --type rsa --dn "CN=github.com/VizzleTF" --outform pem > ~/pki/cacerts/ca-cert.pem;
-echo "done"
+	    --type rsa --dn "CN=github_VizzleTF" --outform pem > ~/pki/cacerts/ca-cert.pem;
+
+echo "#############################"
+sleep 0.1
+echo "####                     ####"
+sleep 0.1
+echo "##                         ##"
+sleep 0.1
+echo "##          Done           ##"
+sleep 0.1
+echo "##                         ##"
+sleep 0.1
+echo "####                     ####"
+sleep 0.1
+echo "#############################"
+sleep 0.5
+echo "#############################"
+sleep 0.1
+echo "####                     ####"
+sleep 0.1
+echo "##                         ##"
+sleep 0.1
+echo "##      Go to step 2!      ##"
+sleep 0.1
+echo "##                         ##"
+sleep 0.1
+echo "####                     ####"
+sleep 0.1
+echo "#############################"
+sleep 0.1
+echo "#############################"
+sleep 0.6
