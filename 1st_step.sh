7c1b0ba1ec8b3d276a4df7e6b7937fa3645955cc
VizzleTF <54704529+VizzleTF@users.noreply.github.com>
2021-01-18 18:52:32 +0300

    Update 1st_step.sh

--- 1st_step.sh
+++ 1st_step.sh
@@ -1,60 +1,90 @@
 #!/bin/bash
 echo "###########################"
-sleep 0.5
+sleep 0.3
 echo "###########################"
-sleep 1
+sleep 0.3
 echo "##########  Hi  ###########"
-sleep 1
+sleep 0.3
 echo "#########  from  ##########"
-sleep 1
+sleep 0.3
 echo "#######  VizzleTF  ########"
-sleep 1
+sleep 0.3
 echo "###########################"
-sleep 1
+sleep 0.3
 echo "###########################"
-sleep 1
-echo "###########################
-#                         #
-#                         #
-# Updating repositories.. #
-#                         #
-#                         #
-###########################"
+sleep 0.1
+echo "###########################"
+sleep 0.1
+echo "#                         #"
+sleep 0.1
+echo "#                         #"
+sleep 0.1
+echo "# Updating repositories.. #"
+sleep 0.1
+echo "#                         #"
+sleep 0.1
+echo "#                         #"
+sleep 0.1
+echo "###########################"
 
 apt-get update >> /dev/null;
 
-echo "###########################
-#                         #
-#                         #
-#          Done           #
-#                         #
-#                         #
-###########################"
+echo "###########################"
+sleep 0.1
+echo "#                         #"
+sleep 0.1
+echo "#                         #"
+sleep 0.1
+echo "#          Done           #"
+sleep 0.1
+echo "#                         #"
+sleep 0.1
+echo "#                         #"
+sleep 0.1
+echo "###########################"
 
-echo "###########################
-#                         #
-#                         #
-# Installing StrongSwan.. #
-#                         #
-#                         #
-###########################"
+echo "###########################"
+sleep 0.1
+echo "#                         #"
+sleep 0.1
+echo "#                         #"
+sleep 0.1
+echo "# Installing StrongSwan.. #"
+sleep 0.1
+echo "#                         #"
+sleep 0.1
+echo "#                         #"
+sleep 0.1
+echo "###########################"
 
 apt-get install --assume-yes strongswan strongswan-pki libcharon-extra-plugins > /dev/null;
 
-echo "###########################
-#                         #
-#                         #
-#          Done           #
-#                         #
-#                         #
-###########################"
-sleep 3
-echo "###########################
-#                         #
-#                         #
-#      Go to step 2!      #
-#                         #
-#                         #
-###########################"
+echo "###########################"
+sleep 0.1
+echo "#                         #"
+sleep 0.1
+echo "#                         #"
+sleep 0.1
+echo "#          Done           #"
+sleep 0.1
+echo "#                         #"
+sleep 0.1
+echo "#                         #"
+sleep 0.1
+echo "###########################"
+sleep 0.3
+echo "###########################"
+sleep 0.1
+echo "#                         #"
+sleep 0.1
+echo "#                         #"
+sleep 0.1
+echo "#      Go to step 2!      #"
+sleep 0.1
+echo "#                         #"
+sleep 0.1
+echo "#                         #"
+sleep 0.1
+echo "###########################"
 sleep 1
 
